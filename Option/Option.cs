69cb52b5ef49712ba982521f54966591fee62905
nnuuvv <github@nuv.direct>
2025-03-19 17:07:43 +0100

    implement basic option and result types

---
+++ Option/Option.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public record Option<T>
+{
+    /// <summary>
+    /// May or may not have a value, check HasValue.
+    /// <remarks>
+    ///     <para>
+    ///     !! Should not be accessed directly outside the Option class
+    ///     <para>
+    ///     Use <see cref="Option.Unwrap(Option{T},x)"/> instead
+    ///     </para>
+    ///     </para>
+    /// </remarks>
+    /// </summary>
+    public T Value { get; set; }
+
+    /// <summary>
+    /// Indicates whether Value is set.
+    /// <remarks>
+    ///     <para>
+    ///     !! Should not be accessed directly outside the Option class
+    ///     <para>
+    ///     Use <see cref="Option.IsSome(Option{T})"/> or <see cref="Option.IsNone(Option{T})"/> instead
+    ///     </para>
+    ///     </para>
+    /// </remarks>
+    /// </summary>
+    public bool HasValue { get; set; }
+}
+
+public static class Option
+{
+    public static bool IsSome<T>(this Option<T> option) => option.HasValue;
+    public static bool IsNone<T>(this Option<T> option) => !option.HasValue;
+
+    public static Option<T> Some<T>(T value)
+    {
+        return new Option<T>()
+        {
+            HasValue = true,
+            Value = value
+        };
+    }
+
+    public static Option<T> None<T>()
+    {
+        return new Option<T>()
+        {
+            HasValue = false,
+        };
+    }
+
+    /// <summary>
+    /// Extracts the value from an 'Option', returning a default value if there is none.
+    /// </summary>
+    /// <param name="option"></param>
+    /// <param name="defaultValue"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static T Unwrap<T>(this Option<T> option, T defaultValue)
+    {
+        return option.IsSome() switch
+        {
+            true => option.Value,
+            _ => defaultValue
+        };
+    }
+
+    /// <summary>
+    /// Updates a value held within the 'Some' of an 'Option' by calling a given function on it.
+    ///
+    /// If the 'Option' is a 'None' rather than 'Some', the function is not called and the 'Option' stays the same.
+    /// </summary>
+    /// <param name="option"></param>
+    /// <param name="action"></param>
+    /// <typeparam name="TSource"></typeparam>
+    /// <typeparam name="TResult"></typeparam>
+    /// <returns></returns>
+    public static Option<TResult> Map<TSource, TResult>(this Option<TSource> option, Func<TSource, TResult> action)
+    {
+        return option.IsSome() switch
+        {
+            true => Some(action(option.Value)),
+            _ => None<TResult>()
+        };
+    }
+
+
+    /// <summary>
+    /// Merges a nested 'option' into a single layer.
+    /// </summary>
+    /// <param name="option"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static Option<T> Flatten<T>(this Option<Option<T>> option)
+    {
+        return option.IsSome() switch
+        {
+            true => option.Value,
+            _ => None<T>()
+        };
+    }
+
+    /// <summary>
+    /// Updates a value held within the `Some` of an `Option` by calling a given function
+    /// on it, where the given function also returns an `Option`. The two options are
+    /// then merged together into one `Option`.
+    ///
+    /// If the `Option` is a `None` rather than `Some` the function is not called and the
+    /// option stays the same.
+    ///
+    /// This function is the equivalent of calling `map` followed by `flatten`, and
+    /// it is useful for chaining together multiple functions that return `Option`.
+    /// </summary>
+    /// <param name="option"></param>
+    /// <param name="apply"></param>
+    /// <typeparam name="TSource"></typeparam>
+    /// <typeparam name="TResult"></typeparam>
+    /// <returns></returns>
+    public static Option<TResult> Then<TSource, TResult>(this Option<TSource> option,
+        Func<TSource, Option<TResult>> apply)
+    {
+        return option.IsSome() switch
+        {
+            true => apply(option.Value),
+            _ => None<TResult>()
+        };
+    }
+
+    /// <summary>
+    /// Combines a list of 'Option's into a single 'Option'.
+    /// If all elements in the list are 'Some' then returns a 'Some' holding the list of values.
+    /// If any element is 'None' then returns 'None'.
+    /// </summary>
+    /// <param name="options"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static Option<List<T>> All<T>(this List<Option<T>> options)
+    {
+        var list = new List<T>();
+
+        foreach (var x in options)
+        {
+            if (x.IsNone()) return None<List<T>>();
+            list.Add(x.Value);
+        }
+
+        return Some(list);
+    }
+
+    public static List<T> Values<T>(this List<Option<T>> list)
+    {
+        return list
+            .Where(x => x.HasValue)
+            .Select(x => x.Value)
+            .ToList();
+    }
+}
---
+++ Result/Result.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// Result represents the result of something that may succeed or not.
+/// 'Ok' means it was successful, 'Error' means it was not successful.
+/// </summary>
+/// <typeparam name="TValue">Type of the value</typeparam>
+/// <typeparam name="TError">Type of the error</typeparam>
+public record Result<TValue, TError>
+{
+    /// <summary>
+    /// May or may not have a value, check IsOk.
+    /// <remarks>
+    ///     <para>
+    ///     !! Should not be accessed directly outside the Option class
+    ///     <para>
+    ///     Use <see cref="Result.Unwrap(Result{TValue,TError},x)"/> instead
+    ///     </para>
+    ///     </para>
+    /// </remarks>
+    /// </summary>
+    public TValue Value { get; set; }
+
+    /// <summary>
+    /// Indicates whether Value is set.
+    /// <remarks>
+    ///     <para>
+    ///     !! Should not be accessed directly outside the Result class
+    ///     <para>
+    ///     Use <see cref="Result.IsOk(Result{TValue,TError})"/> or <see cref="Result.IsError(Result{TValue,TError})"/> instead
+    ///     </para>
+    ///     </para>
+    /// </remarks>
+    /// </summary>
+    public bool DidSucceed { get; set; }
+
+    /// <summary>
+    /// May or may not have a value, check IsOk.
+    /// <remarks>
+    ///     <para>
+    ///     !! Should not be accessed directly outside the Option class
+    ///     <para>
+    ///     Use <see cref="Result.UnwrapError(Result{TValue,TError},x)"/> instead
+    ///     </para>
+    ///     </para>
+    /// </remarks>
+    /// </summary>
+    public TError ErrorValue { get; set; }
+}
+
+public static class Result
+{
+    public static bool IsOk<TValue, TError>(this Result<TValue, TError> result) => result.DidSucceed;
+    public static bool IsError<TValue, TError>(this Result<TValue, TError> result) => !result.DidSucceed;
+
+    public static Result<TValue, TError> Ok<TValue, TError>(TValue value)
+    {
+        return new Result<TValue, TError>()
+        {
+            DidSucceed = true,
+            Value = value
+        };
+    }
+
+    public static Result<TValue, TError> Error<TValue, TError>(TError error)
+    {
+        return new Result<TValue, TError>()
+        {
+            DidSucceed = false,
+            ErrorValue = error
+        };
+    }
+
+    /// <summary>
+    /// Updates a value held within the 'Ok' of a 'Result' by calling a given function on it.
+    /// 
+    /// If the 'Result' is an 'Error' rather than 'Ok', the function is not called and the 'Result' stays the same.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="action"></param>
+    /// <typeparam name="TSource"></typeparam>
+    /// <typeparam name="TResult"></typeparam>
+    /// <typeparam name="TError"></typeparam>
+    /// <returns></returns>
+    public static Result<TResult, TError> Map<TSource, TResult, TError>(this Result<TSource, TError> input,
+        Func<TSource, TResult> action)
+    {
+        return input.IsOk() switch
+        {
+            true => Ok<TResult, TError>(action(input.Value)),
+            false => Error<TResult, TError>(input.ErrorValue),
+        };
+    }
+
+    /// <summary>
+    /// Extracts the value from a 'Result', returning a default value if there is none.
+    /// </summary>
+    /// <param name="result"></param>
+    /// <param name="defaultValue"></param>
+    /// <typeparam name="TValue"></typeparam>
+    /// <typeparam name="TError"></typeparam>
+    /// <returns></returns>
+    public static TValue Unwrap<TValue, TError>(this Result<TValue, TError> result, TValue defaultValue)
+    {
+        return result.IsOk() switch
+        {
+            true => result.Value,
+            _ => defaultValue
+        };
+    }
+
+    /// <summary>
+    /// Merges a nested 'result' into a single layer.
+    /// </summary>
+    /// <param name="result"></param>
+    /// <typeparam name="TValue"></typeparam>
+    /// <typeparam name="TError"></typeparam>
+    /// <returns></returns>
+    public static Result<TValue, TError> Flatten<TValue, TError>(this Result<Result<TValue, TError>, TError> result)
+    {
+        return result.IsOk() switch
+        {
+            true => result.Value,
+            _ => Error<TValue, TError>(result.ErrorValue)
+        };
+    }
+
+    /// <summary>
+    /// Updates a value held within the `Ok` of a `Result` by calling a given function
+    /// on it, where the given function also returns a `Result`. The two results are
+    /// then merged together into one `Result`.
+    /// 
+    /// If the `Result` is an `Error` rather than `Ok` the function is not called and the original 'Error' is returned.
+    /// 
+    /// This function is the equivalent of calling `map` followed by `flatten`, and
+    /// it is useful for chaining together multiple functions that return `Result`.
+    /// </summary>
+    /// <param name="result"></param>
+    /// <param name="apply"></param>
+    /// <typeparam name="TSource"></typeparam>
+    /// <typeparam name="TResult"></typeparam>
+    /// <typeparam name="TError"></typeparam>
+    /// <returns></returns>
+    public static Result<TResult, TError> Try<TSource, TResult, TError>(this Result<TSource, TError> result,
+        Func<TSource, TResult> apply)
+    {
+        return result.IsOk() switch
+        {
+            true => Ok<TResult, TError>(apply(result.Value)),
+            _ => Error<TResult, TError>(result.ErrorValue)
+        };
+    }
+
+    /// <summary>
+    /// Updates a value held within the 'Error' of a result by calling a given function on it.
+    /// 
+    /// If the result is 'Ok' rather than 'Error' the function won't be called.
+    /// But the value will be transferred into a new 'Result' with the same Error type.
+    /// Since the returned 'Error' types need to match (C# limitation)
+    /// </summary>
+    /// <param name="result"></param>
+    /// <param name="action"></param>
+    /// <typeparam name="TValue"></typeparam>
+    /// <typeparam name="TErrorSource"></typeparam>
+    /// <typeparam name="TErrorResult"></typeparam>
+    /// <returns></returns>
+    public static Result<TValue, TErrorResult> MapError<TValue, TErrorSource, TErrorResult>(
+        this Result<TValue, TErrorSource> result, Func<TErrorSource, TErrorResult> action)
+    {
+        return result.IsOk() switch
+        {
+            true => Ok<TValue, TErrorResult>(result.Value),
+            false => Error<TValue, TErrorResult>(action(result.ErrorValue)),
+        };
+    }
+
+    /// <summary>
+    /// Extracts the 'Error' value from a result, returning a default value if the result is an 'Ok'.
+    /// </summary>
+    /// <param name="result"></param>
+    /// <param name="defaultError"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TError"></typeparam>
+    /// <returns></returns>
+    public static TError UnwrapError<T, TError>(this Result<T, TError> result, TError defaultError)
+    {
+        return result.IsOk() switch
+        {
+            false => result.ErrorValue,
+            _ => defaultError,
+        };
+    }
+
+    /// <summary>
+    /// Extracts the inner value from a result. Both the value and error must be of the same type.
+    /// </summary>
+    /// <param name="result"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static T UnwrapBoth<T>(this Result<T, T> result)
+    {
+        return result.IsOk() switch
+        {
+            true => result.Value,
+            false => result.ErrorValue
+        };
+    }
+
+    /// <summary>
+    /// Combines a list of results into a single result.
+    /// If all elements in the list are 'Ok' then returns an 'Ok' holding the list of values.
+    /// If any element is 'Error' then returns the first error.
+    /// </summary>
+    /// <param name="results"></param>
+    /// <typeparam name="TValue"></typeparam>
+    /// <typeparam name="TError"></typeparam>
+    /// <returns></returns>
+    public static Result<List<TValue>, TError> All<TValue, TError>(this List<Result<TValue, TError>> results)
+    {
+        var list = new List<TValue>();
+
+        foreach (var result in results)
+        {
+            if (result.IsError()) return Error<List<TValue>, TError>(result.ErrorValue);
+            list.Add(result.Value);
+        }
+
+        return Ok<List<TValue>, TError>(list);
+    }
+
+    /// <summary>
+    /// Given a list of results, returns a pair where the first element is a list of all the values inside 'Ok' and the second element is a list with all the values inside 'Error'.
+    /// </summary>
+    /// <param name="results"></param>
+    /// <typeparam name="TValue"></typeparam>
+    /// <typeparam name="TError"></typeparam>
+    /// <returns></returns>
+    public static (List<TValue>, List<TError>) Partition<TValue, TError>(this List<Result<TValue, TError>> results)
+    {
+        var values = new List<TValue>();
+        var errors = new List<TError>();
+
+        foreach (var result in results)
+        {
+            if(result.IsOk()) values.Add(result.Value);
+            if(result.IsError()) errors.Add(result.ErrorValue);
+        }
+
+        return (values, errors);
+    }
+
+
+    public static List<T> Values<T, TError>(this List<Result<T, TError>> list)
+    {
+        return list
+            .Where(x => x.DidSucceed)
+            .Select(x => x.Value)
+            .ToList();
+    }
+}
